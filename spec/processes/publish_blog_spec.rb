d12bd671be72c69f296659119afb97cf2b083abc
argonus <argonuspiotr@gmail.com>
2019-01-13 15:37:36 +0100

    Do not publish blog twice

--- spec/processes/publish_blog_spec.rb
+++ spec/processes/publish_blog_spec.rb
@@ -30,6 +30,38 @@ RSpec.describe "Publish Blog Process" do
     expect(blog.published).to be_truthy
   end
 
+  it "does not publishes blog twice" do
+    blog_user = BlogManagement::UserReadModel.create!(name: "Name")
+    blog_article = BlogManagement::ArticleReadModel.create!(title: "A", user_id: blog_user.id)
+    blog_id = SecureRandom.uuid
+
+    command_bus.call(Blogging::BlogCreateCommand.new(
+      blog_id: blog_id,
+      name: "Random",
+      user_id: blog_user.id
+    ))
+    command_bus.call(Blogging::BlogPublishCommand.new({
+      blog_id: blog_id
+    }))
+
+    blog = BlogManagement::BlogReadModel.find_by(id: blog_id)
+    expect(blog.published).to be_truthy
+
+    expect_any_instance_of(Blogging::BlogPublishCommand).not_to receive(:new)
+    command_bus.call(Blogging::ArticlePublishCommand.new(
+      blog_id: blog_id,
+      article_id: blog_article.id,
+      user_id: blog_user.id
+    ))
+
+    blog_article.reload
+    blog.reload
+    expect(blog_article.state).to eq "published"
+    expect(blog.published).to be_truthy
+  end
+
+  private
+
   def command_bus
     Rails.configuration.command_bus
   end
